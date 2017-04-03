1b5b8f265bd56f520f14b4b699a4dc155ade004f
laurenvachon <laurenvachon@gmail.com>
2017-04-03 00:14:07 -0400

    Add files via upload

---
+++ Codeout/js/script.js
@@ -0,0 +1,61 @@
+$( document ).ready(function() {
+
+  // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('header').removeClass('nav-down').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('header').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+
+        lastScrollTop = st;
+    }
+
+
+  // show side nav on scroll to section 2
+  $(window).scroll(function() {
+     var hT = $('#section2').offset().top,
+         hH = $('#section2').outerHeight(),
+         wH = $(window).height(),
+         wS = $(this).scrollTop();
+      console.log((hT-wH) , wS);
+     if (wS > (hT+hH-wH)){
+      $('.navbar').css('opacity','1');
+     }
+  });
+
+  // AOS
+  AOS.init({
+    duration: 1200,
+  })
+
+
+});
