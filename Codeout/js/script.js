ccba81600790298669b7576b22c0c9a957b630e9
LaurenVachon <LaurenVachon@Michaelas-MacBook-Pro.local>
2017-04-05 23:15:34 -0400

    fancybox 3

--- Codeout/js/script.js
+++ Codeout/js/script.js
@@ -78,53 +78,12 @@ if($("body").hasClass("photography")){
     });
   })
 
-  $(".fancybox").fancybox();
-
-  (function ($, F) {
-      F.transitions.resizeIn = function() {
-          var previous = F.previous,
-              current  = F.current,
-              startPos = previous.wrap.stop(true).position(),
-              endPos   = $.extend({opacity : 1}, current.pos);
-
-          startPos.width  = previous.wrap.width();
-          startPos.height = previous.wrap.height();
-
-          previous.wrap.stop(true).trigger('onReset').remove();
-
-          delete endPos.position;
-
-          current.inner.hide();
-
-          current.wrap.css(startPos).animate(endPos, {
-              duration : current.nextSpeed,
-              easing   : current.nextEasing,
-              step     : F.transitions.step,
-              complete : function() {
-                  F._afterZoomIn();
-
-                  current.inner.fadeIn("fast");
-              }
-          });
-      };
-
-  }(jQuery, jQuery.fancybox));
-
-  $(".fancybox")
-      .attr('rel', 'gallery')
-      .fancybox({
-          nextMethod : 'resizeIn',
-          nextSpeed  : 250,
-          padding: 0,
-
-          prevMethod : false,
-
-          helpers : {
-              title : {
-                  type : 'inside'
-              }
-          }
-      });
+  // fancybox
+  $("[data-fancybox]").fancybox({
+  	image : {
+  		protect : true
+  	}
+  });
 
 }
 
